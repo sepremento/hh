471df262dd577600e37becf466ce6362eb40fcfe
Some Name <sepremento@gmail.com>
2020-11-27 02:02:23 +0500

    add vacancies_url_generator, refactor get_contents

--- new_parser.py
+++ new_parser.py
@@ -1,10 +1,14 @@
+import sys
 import re
 import argparse
 import json
 import time
+from urllib.parse import urljoin
 
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
+
+HH_URL = "https://ekaterinburg.hh.ru/search/vacancy"
 
 parser = argparse.ArgumentParser()
 
@@ -18,21 +22,18 @@ def get_vacancies_pagelist(vacancy_name):
         vacancy_name - строка, возможно с пробелами, название вакансии
     Возвращает:
         main_soup - объект BeautifulSoup дла последующей обработки
+        session - сессия для дальнейшей передачи
     """
     print("Обрабатываю вакансию {} ...".format(vacancy_name))
-
     vacancy_name = "+".join(vacancy_name.split())
-    url = "https://ekaterinburg.hh.ru/search/vacancy?text=" + vacancy_name
 
     session = HTMLSession()
-    page = session.get(url)
+    page = session.get(HH_URL, params={'text': vacancy_name})
     main_soup = BeautifulSoup(page.content, 'html.parser')
 
-    return main_soup
+    return main_soup, session
 
-
-    parse_user_choice = input()
-    if parse_user_choice.lower() in ["y", "yes", "д", "да"]:
+"""
         print("Обрабатываю...")
         # Здесь проблема в том, что вакансии записываются в переменную только
         # после того, как список будет пройден. Это создаёт уязвимость в
@@ -45,7 +46,7 @@ def get_vacancies_pagelist(vacancy_name):
             make_json(vacancies)
         else:
             print("Выхожу без сохранения")
-    return
+    return"""
 
 
 def make_json(vacancies_list):
@@ -70,91 +71,67 @@ def get_vac_num(soup):
     return 0
 
 
-def vacancies_generator(main_soup):
-    while main_soup:
+def vacancies_url_generator(main_soup, session):
+    """Генератор, позволяющий получать по одной вакансии с указанной страницы.
+    Генератор находит кнопку перехода на следующую страницу и пытается пройтись
+    по всем вакансиям.
 
+    Аргументы:
+        main_soup - объект BeautifulSoup, с которого начинается поиск вакансий
+        session - живое подключение
 
-def parse_vacancies(main, total_vac, session):
-    """Процедура обработки списка вакансий, возвращает список словарей различных характеристик
-    вакансий."""
-    
+    Поставляет:
+        vacancy (Tag) - тэг вакансии.
+    """
     if session is None:
         session = HTMLSession()
-    
-    page_num = 1
-    vacancies = []
-    while main:
-        print('Считываем страницу', page_num, '/', (int(total_vac) // 50)+1, '...')
-        items = main.find_all('div', attrs = {'class':'vacancy-serp-item__info'})
-        for item in items:
-            vac_name = item.find('a', attrs={'class':'bloko-link HH-LinkModifier',
-                                         'data-qa':'vacancy-serp__vacancy-title'})
-            # пытаемся получить описание вакансии
-            try:
-                vacancy = get_vacancy_contents(vac_name['href'], session)
-                vacancy['href'] = vac_name['href']
-                vacancies.append(vacancy)
-            except KeyboardInterrupt:
-                print("Обработка досрочно прекращена пользователем")
-                return vacancies
+
+    while main_soup:
+        vacancies = main_soup.select('.HH-LinkModifier')
+        for vacancy in vacancies:
+            yield vacancy
+
         try:
-            next_page = main.find('a', attrs = {'class':'HH-Pager-Controls-Next'})
-            url_next = "https://ekaterinburg.hh.ru" + next_page['href']
-            time.sleep(3)
-            page_num += 1
-            page = session.get(url_next)
-            main = BeautifulSoup(page.content, 'html.parser')
+            next_page_href = main_soup.select('.HH-Pager-Controls-Next')[0]['href']
+            url = urljoin(HH_URL, next_page_href)
+            page = session.get(url)
+            main_soup = BeautifulSoup(page.content, 'html.parser')
         except:
-            print('Парсинг закончен')
-            print('-------------------------')
-            print('Всего вакансий:', total_vac)
+            print("Закончили...")
             break
-    return vacancies
-    
-    
+
+
 def get_tags_list(soup):
     """Извлечь из страницы с вакансией список ключевых навыков, указанных в вакансии"""
-    
-    spans = soup.find_all('span', {'data-qa':'bloko-tag__text'})
+    spans = soup.select('.bloko-tag__section_text')
     tags = [span.get_text() for span in spans]
     return tags
 
 
 def get_salary(soup):
     """Извлечь зарплату со страницы"""
-    
-    salary = soup.find('p', {'class':'vacancy-salary'}).get_text()
-    return salary
+    return soup.select('.vacancy-salary')[0].get_text()
 
 
 def get_vac_name(soup):
     """Извлечь название вакансии"""
-    
-    vac_name = soup.find('h1', attrs={'class':'bloko-header-1',
-                                      'data-qa':'vacancy-title'})
-    return vac_name.get_text()
+    return soup.select('h1.bloko-header-1')[0].get_text()
 
 
-def get_vac_id(soup):
+def get_vac_id(vac_url):
     """Извлечь ID вакансии"""
-    
-    vac_id_source = soup.find('a', attrs={'class':'bloko-button bloko-button_secondary bloko-button_stretched',
-                                          'data-qa':'vacancy-response-link-top'})
     pattern= re.compile('\d+')
-    vac_id = re.search(pattern, vac_id_source['href'])[0]
-    return vac_id
+    return re.search(pattern, vac_url)[0]
 
 
 def get_exp(soup):
     """Извлечь требуемый опыт работы"""
-    
     experience = soup.find('span', {'data-qa':'vacancy-experience'})
     return experience.get_text()
 
 
 def get_contacts(soup):
     """Извлечь контакты рекрутера"""
-    
     try:
         fio = soup.find('p', {'data-qa':'vacancy-contacts__fio'}).get_text()
     except: fio = ''
@@ -164,21 +141,18 @@ def get_contacts(soup):
     try:
         email = soup.find('a', {'data-qa':'vacancy-contacts__email'}).get_text()
     except: email = ''
-    
     contacts_dict = dict(fio=fio, phone=phone, email=email)
     return contacts_dict
 
 
 def get_company(soup):
     """Извлечь название компании"""
-    
     company_name = soup.find('span', {'class':'bloko-section-header-2 bloko-section-header-2_lite'})
     return company_name.get_text()
 
 
 def get_description(soup):
     """Извлечь текстовое описание вакансии"""
-    
     non_branded = soup.find('div', {'data-qa':'vacancy-description'})
     branded = soup.find('div', {'class':'vacancy-section HH-VacancyBrandedDescription-DANGEROUS-HTML'})
     description = non_branded or branded
@@ -187,19 +161,21 @@ def get_description(soup):
 
 def get_timestamp(soup):
     """Извлечь дату создания вакансии"""
-    
     vac_timestamp = soup.find('p', class_="vacancy-creation-time")
     return vac_timestamp.get_text()
 
 
 def get_vacancy_contents(vac_url, session):
-    """Возвращает словарь из элементов описания вакансии"""
-    
-    time.sleep(3)  #  чтобы нас не остановили боты сайта, подождём какое-то время
+    """Возвращает словарь из элементов описания вакансии
+    Аргументы:
+        vac_url (str) - адрес вакансии
+        session (HTMLSession) - открытое соединение
+    """
+    # time.sleep(3)  #  чтобы нас не остановили боты сайта, подождём какое-то время
     vac_page = session.get(vac_url)
     soup = BeautifulSoup(vac_page.content, 'html.parser')
-        
-    vac_id = get_vac_id(soup)
+
+    vac_id = get_vac_id(vac_url)
     vac_name = get_vac_name(soup)
     company = get_company(soup)
     tags = get_tags_list(soup)
@@ -212,15 +188,31 @@ def get_vacancy_contents(vac_url, session):
     description = get_description(soup)
     timestamp = get_timestamp(soup)
     print("Обработана вакансия: {}".format(vac_name))
-    
-    return dict(vac_id=vac_id, vac_name=vac_name, 
-               company=company, tags=tags, salary=salary,
-               fio=fio, phone=phone, email=email, exp=exp, 
-               description=description, timestamp=timestamp)
+
+    return dict(
+            vac_id=vac_id,
+            vac_name=vac_name,
+            company=company,
+            tags=tags,
+            salary=salary,
+            fio=fio,
+            phone=phone,
+            email=email,
+            exp=exp,
+            description=description,
+            timestamp=timestamp)
 
 
 if args.vacancy is not None:
-    main_soup = get_vacancies_pagelist(args.vacancy)
+    main_soup, session = get_vacancies_pagelist(args.vacancy)
     total_vac = get_vac_num(main_soup)
+
     print("По этому запросу найдено вакансий: {}".format(total_vac))
     print("Обработать эти вакансии? y/[n]\n")
+    parse_user_choice = input()
+    if parse_user_choice.lower() not in ["y", "yes", "д", "да"]:
+        sys.exit()
+    for vacancy in vacancies_url_generator(main_soup, session):
+        vacancy_contents = get_vacancy_contents(vacancy['href'], session)
+        print(vacancy_contents)
+        break
